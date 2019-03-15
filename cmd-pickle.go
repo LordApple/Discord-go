723008d440ad3818f6628bc7150626a9c5a6d9d8
Apple <lordapplz@protonmail.com>
2019-03-15 23:54:55 +0100

    added sorting, this took way longer then it should've.

--- cmd-pickle.go
+++ cmd-pickle.go
@@ -3,27 +3,47 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sort"
 	"strconv"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
 )
 
+type byLength []string
+
+func (s byLength) Len() int {
+	return len(s)
+}
+func (s byLength) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func (s byLength) Less(i, j int) bool {
+	return len(s[i]) < len(s[j])
+}
+
 func pickle(users []*discordgo.User) string {
-	dongs := make(map[string]string)
+	exists := make(map[string]string)
+	dongs := []string{}
+	names := []string{}
 	msg := ""
 
 	for _, user := range users {
-		if _, isExist := dongs[user.Username]; isExist {
+		if _, isExist := exists[user.Username]; isExist {
 			continue
 		}
 		uID, _ := strconv.ParseInt(user.ID, 10, 64)
 		rand.Seed(uID)
-		dongs[user.Username] = fmt.Sprintf("8%vD", strings.Repeat("=", rand.Intn(30)))
+
+		dongs = append(dongs, fmt.Sprintf("8%vD", fmt.Sprintf(strings.Repeat("=", rand.Intn(30)))))
+		names = append(names, user.Username)
+		exists[user.Username] = ""
 	}
 
-	for name, dong := range dongs {
-		msg += fmt.Sprintf("**%v's size:**\n%v\n", name, dong)
+	sort.Sort(byLength(dongs))
+
+	for x := 0; x < len(names); x++ {
+		msg += fmt.Sprintf("**%v's size:**\n%v\n", names[x], dongs[x])
 	}
 
 	return msg
